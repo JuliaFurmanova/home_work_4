15b33fa24397686b61f87765a79147bc02464ef7
FurmanovaYA <ula55509@mail.ru>
2025-02-12 17:12:43 +0300

    Добавила тест со списками

--- test_data_types.py
+++ test_data_types.py
@@ -1,4 +1,5 @@
 from math import pi
+import random
 
 def test_greeting():
     """
@@ -53,7 +54,9 @@ def test_random_list():
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
     # TODO создайте список
-    l = []
+    l = [random.randint(1, 100) for _ in range(10)]
+    l = sorted(l)
+    print("Отсортированный список:", l)
 
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
